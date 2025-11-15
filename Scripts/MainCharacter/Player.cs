5bb186f59d26349ae832f48296cd4f60bb071db9
Patrick Charon <152666303+pcharon01@users.noreply.github.com>
2025-11-15 12:03:21 +0100

    Player movement this time

---
+++ Scripts/MainCharacter/Player.cs
@@ -0,0 +1,47 @@
+using Godot;
+using System;
+
+public partial class Player: CharacterBody2D
+{
+	private const float GRAVITY = 200.0f;
+	
+	[Export]
+	public float acceleration { get; set; } = 1000.0f;
+	
+	[Export]
+	public float maxSpeed { get; set; } = 500.0f;
+	
+	[Export]
+	public float friction { get; set; } = 0.975f;
+	
+	public override void _Process(double delta) {
+		var velocity = Velocity;
+		
+		if (Input.IsActionPressed("move_right")) {
+			if (velocity.X < maxSpeed) {
+				velocity.X += acceleration * (float) delta;
+			}
+		}
+		
+		if (Input.IsActionPressed("move_left")) {
+			if (velocity.X > -maxSpeed) {
+				velocity.X -= acceleration * (float) delta;
+			}
+		}
+		
+		if (!Input.IsActionPressed("move_left") && !Input.IsActionPressed("move_right")) {
+			velocity.X *= friction;
+		}
+		
+		Velocity = velocity;
+	}
+
+	public override void _PhysicsProcess(double delta) {
+		var velocity = Velocity;
+		velocity.Y += (float)delta * GRAVITY;
+		Velocity = velocity;
+
+		var motion = Velocity * (float)delta;
+		MoveAndSlide();
+	}
+}
