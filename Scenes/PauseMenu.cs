5fbb48324722c5641c903cb7f23874731bc7d61b
Stepan Tyurin <sanstepon5@gmail.com>
2025-11-15 12:22:37 +0100

    Add comic sans ms font for the project

--- Scenes/PauseMenu.cs
+++ Scenes/PauseMenu.cs
@@ -3,17 +3,39 @@ using System;
 
 public partial class PauseMenu : Control
 {
+	[Signal]
+	public delegate void ResumeGameEventHandler();
+	
 	private Button _exitButton;
+	private Button _resumeButton;
+	private Button _settingsButton;
+	
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
 		_exitButton = GetNode<Button>("PanelContainer/MarginContainer/VBoxContainer/ExitButton");
-		_exitButton.Connect("pressed", new Callable(this, "_on_ExitButton_pressed"));
+		_exitButton.Pressed += _onExitButtonPressed;
+		
+		_resumeButton = GetNode<Button>("PanelContainer/MarginContainer/VBoxContainer/ResumeButton");
+		_resumeButton.Pressed += _onResumeButtonPressed;
+		
+		_settingsButton = GetNode<Button>("PanelContainer/MarginContainer/VBoxContainer/SettingsButton");
+		_settingsButton.Pressed += _onSettingsButtonPressed;
 	}
 
-	private void _on_ExitButton_pressed()
+	private void _onExitButtonPressed()
 	{
 		GetTree().Quit();
 	}
 
+	private void _onResumeButtonPressed()
+	{
+		EmitSignal(SignalName.ResumeGame);
+		QueueFree();
+	}
+
+	private void _onSettingsButtonPressed()
+	{
+		GD.Print("Settings opened");
+	}
 }
