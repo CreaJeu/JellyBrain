59a8226714d71d0ceea65674d08cd874947508a0
Fisselo <77895882+Fiss53@users.noreply.github.com>
2026-02-03 22:05:13 +0100

    De la merde

--- Scenes/Prefabs/Enemies/Palourde.cs
+++ Scenes/Prefabs/Enemies/Palourde.cs
@@ -4,17 +4,25 @@ using JellyBrain.Scripts.Utils;
 
 public partial class Palourde : Node2D
 {
-	private AnimatedSprite2D _sprite;
+	private Sprite2D _sprite;
 	private Node2D _player;
 	private Direction _playerDirection; // On which side is player located
 	private SimpleMovementComponent _movementComponent;
+	private Node2D _visualNode2D;// node just under the root node for animation purpose
+	public AnimationPlayer _animationPlayer;
+	
 	
 	public override void _Ready()
 	{
 		
-		_sprite = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
-		_sprite.Animation = "movement";
-		_sprite.Play();
+		_visualNode2D = GetNode<Node2D>("VisualNode2D");
+		_sprite = GetNode<Sprite2D>("VisualNode2D/Sprite2D");
+		
+		
+		// GD.Print("OU EST LA PALOURDE");
+		// GD.Print(Position.X +"   "  +  Position.Y);
+		_animationPlayer = GetNode<AnimationPlayer>("VisualNode2D/AnimationPlayer");
+		_animationPlayer.Play("Jump Left");
 		
 		_movementComponent = GetNode<SimpleMovementComponent>("SimpleMovementComponent");
 		_movementComponent.ChangedDirection += _changeFacingDirection;
@@ -39,7 +47,6 @@ public partial class Palourde : Node2D
 
 	public override void _Process(double delta)
 	{
-		
 		if (_playerInInterestZone())
 		{
 			// Face player when within interest zone
@@ -54,17 +61,17 @@ public partial class Palourde : Node2D
 	private void _onInterestZoneEntered(Node2D body)
 	{
 		_player = body;
-		// _sprite.Animation = "palourdeClosing";
-		// _sprite.Play();
 		_updatePlayerDirection();
+		if (_animationPlayer.CurrentAnimation != "Jump Left")
+		{
+			_animationPlayer.Play("Jump Left");
+		}
 		_movementComponent.SetDirection(_playerDirection);
 	}
 
 	private void _onInterestZoneExited()
 	{
 		_player = null;
-		// _sprite.Animation = "movement";
-		// _sprite.Play();
 	}
 	
 	private bool _playerInInterestZone()
@@ -79,6 +86,5 @@ public partial class Palourde : Node2D
 	
 	private void _changeFacingDirection(Direction newDirection)
 	{
-		_sprite.FlipH = newDirection == Direction.Right;
-	}
+		_visualNode2D.Scale = new Vector2(newDirection == Direction.Left ? 1f : -1f, 1f);	}
 }
