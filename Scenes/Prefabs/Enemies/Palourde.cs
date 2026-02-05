74819e4908dd52663e08ae3057b9393cf5e2c0e0
Fisselo <77895882+Fiss53@users.noreply.github.com>
2026-02-05 13:17:09 +0100

    Addon + ressources + modification on palourde

--- Scenes/Prefabs/Enemies/Palourde.cs
+++ Scenes/Prefabs/Enemies/Palourde.cs
@@ -4,39 +4,35 @@ using JellyBrain.Scripts.Utils;
 
 public partial class Palourde : Node2D
 {
-	private Sprite2D _sprite;
+	private AnimatedSprite2D _sprite;
 	private Node2D _player;
 	private Direction _playerDirection; // On which side is player located
 	private SimpleMovementComponent _movementComponent;
-	private Node2D _visualNode2D;// node just under the root node for animation purpose
-	public AnimationPlayer _animationPlayer;
-	
-	
+
+
+
 	public override void _Ready()
 	{
-		
-		_visualNode2D = GetNode<Node2D>("VisualNode2D");
-		_sprite = GetNode<Sprite2D>("VisualNode2D/Sprite2D");
-		
-		
-		// GD.Print("OU EST LA PALOURDE");
-		// GD.Print(Position.X +"   "  +  Position.Y);
-		_animationPlayer = GetNode<AnimationPlayer>("VisualNode2D/AnimationPlayer");
-		_animationPlayer.Play("Jump Left");
-		
+
+		_sprite = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
+
+		_sprite.Play("move");
+
+
 		_movementComponent = GetNode<SimpleMovementComponent>("SimpleMovementComponent");
 		_movementComponent.ChangedDirection += _changeFacingDirection;
 
-		
+
 		var interestZone = GetNode<InterestZone>("InterestZone");
 		interestZone.InterestedZoneEntered += _onInterestZoneEntered;
 		interestZone.InterestedZoneExited += _onInterestZoneExited;
-		
+
 		if (_movementComponent == null)
 		{
 			GD.PrintErr("Erreur : _movementComponent est null !");
 			return;
 		}
+
 		if (_sprite == null)
 		{
 			GD.PrintErr("Erreur : _sprite est null !");
@@ -54,7 +50,7 @@ public partial class Palourde : Node2D
 			_changeFacingDirection(_playerDirection);
 			_movementComponent.SetDirection(_playerDirection);
 		}
-		
+
 
 	}
 
@@ -62,10 +58,6 @@ public partial class Palourde : Node2D
 	{
 		_player = body;
 		_updatePlayerDirection();
-		if (_animationPlayer.CurrentAnimation != "Jump Left")
-		{
-			_animationPlayer.Play("Jump Left");
-		}
 		_movementComponent.SetDirection(_playerDirection);
 	}
 
@@ -73,7 +65,7 @@ public partial class Palourde : Node2D
 	{
 		_player = null;
 	}
-	
+
 	private bool _playerInInterestZone()
 	{
 		return _player != null;
@@ -83,8 +75,9 @@ public partial class Palourde : Node2D
 	{
 		_playerDirection = _player.Position.X > GlobalPosition.X ? Direction.Right : Direction.Left;
 	}
-	
+
 	private void _changeFacingDirection(Direction newDirection)
 	{
-		_visualNode2D.Scale = new Vector2(newDirection == Direction.Left ? 1f : -1f, 1f);	}
+		_sprite.FlipH = newDirection == Direction.Left;
+	}
 }
