00dab70f8ad4dea98a35e90d39d8b93a5ca03a35
Patrick Charon <152666303+pcharon01@users.noreply.github.com>
2026-02-15 14:04:37 +0100

    Replaced rectangles with hearts in HUD

--- Scripts/Interface/PlayerHud.cs
+++ Scripts/Interface/PlayerHud.cs
@@ -8,45 +8,45 @@ using JellyBrain.Scripts.Utils;
 
 public partial class PlayerHud : Control
 {
-    //should be assigned in editor
-    [Export]
-    public HealthComponent healthComponent;
-    
+	//should be assigned in editor
+	[Export]
+	public HealthComponent healthComponent;
+	
    
 
 
-    
-    //should contain nodes that manage the display of hearts
-    private ColorRect[] hpHeartList;
+	
+	//should contain nodes that manage the display of hearts
+	private TextureRect[] hpHeartList;
 
-    public override void _Ready()
-    {
-        if (healthComponent == null)
-        {
-            throw new NullReferenceException("Health component was not passed to the node");
-        }
-        hpHeartList = CollectionsAlternative.GetChildren<ColorRect>(GetNode<Control>("CanvasLayer/HP"));
-        healthComponent.HealthChanged += _onHealthChanged;
+	public override void _Ready()
+	{
+		if (healthComponent == null)
+		{
+			throw new NullReferenceException("Health component was not passed to the node");
+		}
+		hpHeartList = CollectionsAlternative.GetChildren<TextureRect>(GetNode<Control>("CanvasLayer/HP"));
+		healthComponent.HealthChanged += _onHealthChanged;
 
-    }
+	}
 
 
 
-    private void _onHealthChanged(int oldHealth, int newHealth)
-    {
-        for (int i = 0; i < newHealth; i++)
-        {
-            hpHeartList[i].SetVisible(true);
-        }
+	private void _onHealthChanged(int oldHealth, int newHealth)
+	{
+		for (int i = 0; i < newHealth; i++)
+		{
+			hpHeartList[i].SetVisible(true);
+		}
 
-        for (int i = newHealth; i < hpHeartList.Length; i++)
-        {   
-            hpHeartList[i].SetVisible(false);
-        }
-        //GD.Print($"oldHealth: {oldHealth}, newHealth: {newHealth}");
+		for (int i = newHealth; i < hpHeartList.Length; i++)
+		{   
+			hpHeartList[i].SetVisible(false);
+		}
+		//GD.Print($"oldHealth: {oldHealth}, newHealth: {newHealth}");
 		
-    }
+	}
    
-    
+	
 
 }
